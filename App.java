e9e7a2673b188cef5ac41c7a36d36be805978058
Subbu <msmb4u@gmail.com>
2018-07-17 23:16:47 +0530

    Initial Commit

--- App.java
+++ App.java
@@ -1,9 +1,6 @@
 Class App{
 	pSVM(String args[]){
 		sysout("Hello");
-		syout("Hai");
-		sysout("Added third line by msm1252");
-		sysout("Added fourth line");
 	}
 }
 
--- readme.txt
+++
@@ -1,2 +0,0 @@
-Hello this is read me file only
-I am going to add one more line for pull request at line 2
